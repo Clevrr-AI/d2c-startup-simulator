800cca627897e7c4a21d47a32702fc9b9a81b1d1
Yuvraj <yuvraj.dagur928@gmail.com>
2025-06-15 00:19:28 +0530

    added image link to redirect to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,9 @@ const GameContainer: React.FC = () => {
     <div className="min-h-screen" style={{ background: `url(${import.meta.env.BASE_URL}background.png)`, backgroundSize: 'cover', backgroundPosition: 'bottom', backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed' }}>
       <div className="container mx-auto">
         <div className="container bg-white text-blue-600 mx-auto rounded p-3 flex justify-between">
-          <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Logo" className="w-32 mr-2" />
+          <a href="https://getclevrr.com?utm_source=d2c_simulator&utm_medium=app&utm_campaign=game" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+            <img src={`${import.meta.env.BASE_URL}logo.png`} alt="Logo" className="w-32 mr-2" />
+          </a>
           <div className="flex items-center">
             <h1 className="pixel-text text-4xl me-5 font-bold tracking-tight hidden sm:block">D2C Simulator</h1>
             <h1 className="pixel-text text-4xl me-5 font-bold tracking-tight hidden sm:block">|</h1>
